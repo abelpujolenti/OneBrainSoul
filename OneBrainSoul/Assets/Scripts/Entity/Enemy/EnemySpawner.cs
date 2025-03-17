26c9c03ccc3d02b22f849287d808832163428e33
adri <40923191+adri2711@users.noreply.github.com>
2025-03-17 20:25:40 +0100

    wip spawner

--- OneBrainSoul/Assets/Scripts/Entity/Enemy/EnemySpawner.cs
+++ OneBrainSoul/Assets/Scripts/Entity/Enemy/EnemySpawner.cs
@@ -2,12 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ECS.Entities.AI;
+using Managers;
+using Player;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] bool spawnOnStart = true;
+    [SerializeField] bool spawnOnEnterTrigger = false;
+    public bool canSpawn = true;
     AgentEntity agentEntity;
     private void Awake()
     {
         agentEntity = transform.GetChild(0).GetComponent<AgentEntity>();
+        agentEntity.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        if (spawnOnStart)
+        {
+            Spawn();
+        }
+    }
+
+    public void Spawn()
+    {
+        if (!canSpawn) return;
+        canSpawn = false;
+        var spawnedAgent = Instantiate(agentEntity.transform, transform.transform.position, agentEntity.transform.rotation);
+        spawnedAgent.localScale = agentEntity.transform.localScale;
+        spawnedAgent.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (spawnOnEnterTrigger && other.GetComponent<PlayerCharacterController>() != null)
+        {
+            Spawn();
+        }
     }
 }
