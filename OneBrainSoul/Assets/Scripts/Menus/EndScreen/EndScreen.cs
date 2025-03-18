e09a56e118459a7fde94f1d19bc0a87e7a9a8aa4
abelpujolenti <abel.pujol@enti.cat>
2025-03-18 20:00:57 +0100

    Done

---
+++ OneBrainSoul/Assets/Scripts/Menus/EndScreen/EndScreen.cs
@@ -0,0 +1,13 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Menus.EndScreen
+{
+    public class EndScreen : MonoBehaviour
+    {
+        public void MainMenu()
+        {
+            LoadSceneManager.Instance.GoToMainMenu();
+        }
+    }
+}
