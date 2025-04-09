8b448e8de0c55b3789e93f5e658d6deeecb177f5
abelpujolenti <abel.pujol@enti.cat>
2025-04-09 18:28:34 +0200

    Fov Slider Value

--- OneBrainSoul/Assets/Scripts/Menus/Settings/View/FovSlider.cs
+++ OneBrainSoul/Assets/Scripts/Menus/Settings/View/FovSlider.cs
@@ -1,4 +1,5 @@
 ﻿using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,14 +7,19 @@ namespace Menus.Settings.View
 {
     public class FovSlider : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _text;
+        
         private void Start()
         {
-            GetComponent<Slider>().value = SettingsManager.Instance.GetFOV();
+            float value = SettingsManager.Instance.GetFOV();
+            GetComponent<Slider>().value = value;
+            _text.text = value + "";
         }
 
         public void SetFOV(float fovValue)
         {
             SettingsManager.Instance.SetFOV(fovValue);
+            _text.text = fovValue + "";
             
             if (EventsManager.OnChangeFov == null)
             {
